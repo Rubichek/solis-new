34c0e1b4a2fde8948c74c2abba110bb3c59dfb42
Rubichek <rubik.serega@gmail.com>
2025-04-02 15:53:43 +0300

    add light and dark

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
 document.getElementById("year").textContent = new Date().getFullYear()
+
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.menu');
+
+burger.addEventListener('click', () => {
+  burger.classList.toggle('active');
+  menu.classList.toggle('open');
+});
+
+
+// Color Theme
+const buttonThemeLight = document.querySelector('.theme-button-light')
+const buttonThemeDark = document.querySelector('.theme-button-dark')
+
+buttonThemeLight.addEventListener('click', () => {
+  document.documentElement.dataset['themeName'] = 'light'
+});
+
+buttonThemeDark.addEventListener('click', () => {
+  document.documentElement.dataset['themeName'] = 'dark'
+});
+
+
+
